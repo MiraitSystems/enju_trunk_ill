3f84f11d2d6be009ed42a621bb2f83c693451840
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2014-05-20 18:09:11 +0900

    refs 7228 相互貸借画面遷移時のエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,19 @@
 EnjuTrunkIll::Engine.routes.draw do
 end
+
+Rails.application.routes.draw do
+  resources :inter_library_loans do
+    post :export_loan_lists, :on => :collection
+    post :get_loan_lists, :on => :collection
+    post :pickup, :on => :collection
+    post :pickup_item, :on => :collection
+    post :accept, :on => :collection
+    get :accept_item, :on => :collection
+    get :download_file, :on => :collection
+    post :output, :on => :member
+  end
+
+  resources :items do
+    resources :inter_library_loans
+  end
+end
